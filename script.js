527bc433c5e69d7d1b2aca338ff0ada1b41e0a59
Dennis Ignacio <dsvignacio@gmail.com>
2026-02-14 00:39:54 +0800

    Enhance interactivity and visuals: add confetti effect, update card interactions, and improve header styling

--- script.js
+++ script.js
@@ -14,28 +14,95 @@ const imageListSad = [
 
 jQuery(document).ready(function() {
 
+  const randomIndexHappy = Math.floor(Math.random() * imageListHappy.length);
+  $('#img-popup-happy').attr('src', imageListHappy[randomIndexHappy]);
+  const randomIndexSad = Math.floor(Math.random() * imageListSad.length);
+  $('#img-popup-sad').attr('src', imageListSad[randomIndexSad]);
+
   $('div.card-face').on('click', function() {
     $('div.card-container').toggleClass('open');
     $('body').toggleClass('open');
+
+    if ($('div.card-container').hasClass('open')) {
+      // Function to run if the class is present
+      cardOpen();
+    } else {
+      // Function to run if the class is absent (the 'false' state)
+      cardClosed();
+    }
   })
 
-  $('#imgContainer-yes').hover(
-    function() {
-      const randomIndex = Math.floor(Math.random() * imageListHappy.length);
-      $('#img-popup-happy').attr('src', imageListHappy[randomIndex]);
-    },
-    function() {
+  function cardOpen() {
+    $('#imgContainer-yes').on('click',
+      function() {
+        // Happy Dudu
+        const randomIndex = Math.floor(Math.random() * imageListHappy.length);
+        $('#img-popup-happy').attr('src', imageListHappy[randomIndex]);
+        $('#img-popup-happy').css('display', 'block');
+        $('#img-popup-sad').css('display', 'none');
 
-    }
-  )
+        // Change header
+        $('span.title').text('YEEEEEEY!!!');
 
-  $('#imgContainer-no').hover(
-    function() {
-      const randomIndex = Math.floor(Math.random() * imageListSad.length);
-      $('#img-popup-sad').attr('src', imageListSad[randomIndex]);
-    },
-    function() {
+        // Confetti
+        confettiYesCenter();
+      }
+    )
 
-    }
-  )
+    $('#imgContainer-no').on('click',
+      function() {
+        // Sad Dudu
+        const randomIndex = Math.floor(Math.random() * imageListSad.length);
+        $('#img-popup-sad').attr('src', imageListSad[randomIndex]);
+        $('#img-popup-happy').css('display', 'none');
+        $('#img-popup-sad').css('display', 'block');
+
+        // Change header
+        $('span.title').text('😭😭😭😭😭');
+      }
+    )
+  }
+  
+  // RESET ALL
+  function cardClosed() {
+    $('span.title').text('Happy Valentine\'s Day!');
+    $('#img-popup-happy').css('display', 'none');
+    $('#img-popup-sad').css('display', 'none');
+  }
 })
+
+function confettiYesCenter() {
+  const defaults = {
+  spread: 360,
+  ticks: 100,
+  gravity: 0,
+  decay: 0.94,
+  startVelocity: 30,
+  shapes: ["heart"],
+  colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+};
+
+function shoot() {
+  confetti({
+    ...defaults,
+    particleCount: 50,
+    scalar: 2,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 25,
+    scalar: 3,
+  });
+  
+  confetti({
+    ...defaults,
+    particleCount: 10,
+    scalar: 4,
+  });
+}
+
+setTimeout(shoot, 0);
+setTimeout(shoot, 300);
+setTimeout(shoot, 600);
+}
